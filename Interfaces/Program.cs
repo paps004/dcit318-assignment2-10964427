e0453f6214dc8aff59cb93ab50d985e4e0354ecb
paps004 <137115720+paps004@users.noreply.github.com>
2024-06-16 17:23:54 +0000

    Add files via upload

---
+++ Interfaces/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public interface IMovable
+{
+    void Move();
+}
+
+public class Car : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Car is moving");
+    }
+}
+
+public class Bicycle : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Bicycle is moving");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car car = new Car();
+        Bicycle bicycle = new Bicycle();
+
+        car.Move();
+        bicycle.Move();
+
+        Console.ReadLine(); // Add this to prevent the console from closing immediately
+    }
+}
